94d843df0ef5f6f93317f1e705b9a819894ddf94
Jose Perez <josedperez@ucompensar.edu.co>
2026-05-16 22:28:05 -0500

    Arregle el metodo obtener saludo, para que el escanner quede dentro de la funcion

--- src/hackatonjava/HackatonJava.java
+++ src/hackatonjava/HackatonJava.java
@@ -4,18 +4,28 @@
  */
 package hackatonjava;
 
+import java.util.Scanner;
+
 /**
  *
  * @author josep
  */
+
 public class HackatonJava {
-<<<<<<< HEAD
+    private static Scanner scanner;
+
     //Metodo sin retorno para salundar a una persona.
     public static void saludar(String nombre, int edad) {
         System.out.println("¡Hola " + nombre + "! Tienes " + edad + " años.");
     }
-
+    //Metodo con retorno para obtener un saludo según la hora del día.
     public static String obtenerSaludo(int hora){
+        //Solicitar al usuario que ingrese la hora del día
+        try (Scanner sc = new Scanner(System.in)) {
+            System.out.println("Ingresa la hora del día (0-23): ");
+            hora = sc.nextInt();
+        }
+        //funcion if else para determinar el saludo según la hora del día
         if (hora >= 0 && hora <= 11){
             return "¡Buenos días!";
         }else if (hora >= 12 && hora <= 17){
@@ -23,27 +33,23 @@ public class HackatonJava {
         }else if (hora >= 18 && hora <= 23){
             return "¡Buenas noches!";
         }
+        //por si el usuario ingresa una hora no válida, se le indicará que la hora no es válida.
         return "Hora no válida.";
-        try (Scanner scanner = new Scanner(System.in)) {
-        int hora1 = scanner.nextInt();
-            System.out.println(obtenerSaludo(hora1));
-     }
     }
-=======
->>>>>>> parent of 96b6c92 (featIntroduccion de las funciones Static void saludar y String Obtener saludo)
 
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-<<<<<<< HEAD
-       saludar("Jose", 21);
-       
-       System.out.println("Ingresa la hora del día (0-23): ");
-    saludar.accept(obtenerSaludo.apply(hora))
-=======
-        // TODO code application logic here
->>>>>>> parent of 96b6c92 (featIntroduccion de las funciones Static void saludar y String Obtener saludo)
-    }
+
     
+    public static void main(String[] args) {
+        //llamada del metodo saludar para saludar a una persona con su nombre y edad. sin retorno
+       saludar("Jose", 21);
+
+       //llamada del metodo obtenerSaludo para obtener un saludo según la hora del día. con retorno
+       String obtenerHora = obtenerSaludo(0);
+         System.out.println(obtenerHora);
+       
+    
+    }
 }
