7b24a5548964affcd2291ed13927e1e36dce8c3a
Jose Perez <josedperez@ucompensar.edu.co>
2026-05-17 09:03:19 -0500

    agregue la combinacion de menu

--- src/hackatonjava/HackatonJava.java
+++ src/hackatonjava/HackatonJava.java
@@ -13,6 +13,31 @@ import java.util.Scanner;
 
 public class HackatonJava {
 
+    //menu para llamar las funciones por separado
+    public static void menu(){
+        System.out.println("Elija una opción:");
+        System.out.println("1. Saludar");
+        System.out.println("2. Obtener saludo");
+        System.out.println("3. Saludar completo");
+        try (Scanner sc = new Scanner(System.in)) {
+            int opcion = sc.nextInt();
+            switch (opcion) {
+                case 1:
+                    saludar("Jose", 21);
+                    break;
+                case 2:
+                    String obtenerHora = obtenerSaludo(0);
+                    System.out.println(obtenerHora);
+                    break;
+                case 3:
+                    saludarCompleto("Maria", 30, 0);
+                    break;
+                default:
+                    System.out.println("Opción no válida.");
+            }
+        }
+    }
+
     //Metodo sin retorno para salundar a una persona.
     public static void saludar(String nombre, int edad) {
         System.out.println("¡Hola " + nombre + "! Tienes " + edad + " años.");
@@ -35,6 +60,15 @@ public class HackatonJava {
         //por si el usuario ingresa una hora no válida, se le indicará que la hora no es válida.
         return "Hora no válida.";
     }
+     public static void saludarCompleto(String nombre, int edad, int hora){
+        //llamando al metodo void 
+        saludar(nombre, edad);
+    
+        //Llamando al metodo con retorno para conbinarlo en la función
+        String saludo = obtenerSaludo(hora);
+        System.out.println(saludo);
+     }
+
 
     /**
      * @param args the command line arguments
@@ -46,9 +80,11 @@ public class HackatonJava {
        saludar("Jose", 21);
 
        //llamada del metodo obtenerSaludo para obtener un saludo según la hora del día. con retorno
-       String obtenerHora = obtenerSaludo(0);
-         System.out.println(obtenerHora);
+      /*String obtenerHora = obtenerSaludo(0);
+         System.out.println(obtenerHora);*/
        
-    
+       //llamado al metodo saludar completo
+         saludarCompleto("Maria", 30, 0);  
+
     }
 }
