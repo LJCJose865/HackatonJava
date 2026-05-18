dfaef77f02d84017dce6d83d9ae9ca48e1af2183
Kata <akatalinafajardo@ucompensar.edu.co>
2026-05-18 17:57:11 -0500

    Create HackatonJava5.java
    avance inventario modular

---
+++ src/hackatonjava/HackatonJava5.java
@@ -0,0 +1,13 @@
+public class HackatonJava5 {
+// ----------------------------------------//
+// SISTEMA DE INVENTARIO MODULAR RETO 05  //
+//----------------------------------------//
+
+public satatic void main(String[] args) {
+
+String[] productos = {"Laptop","Mouse","Teclado","Monitor","Audífonos"};
+int[] precios = {2500000,45000,120000,890000,200000};
+  probarInventario(productos, precios);}
+  
+public static void mostrarInventario(String[] productos, int[] precios){
+  
